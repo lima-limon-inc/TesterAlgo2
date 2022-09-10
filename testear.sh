4ce2e86f5b3dbb7f2f59733ecbe0dfaee71a88c6
Fabri <moboficial00@gmail.com>
2022-09-10 17:55:37 -0300

    Lista la parte de los modulos. Ahora tambien te lo hace automaticamente

--- testear.sh
+++ testear.sh
@@ -2,6 +2,7 @@
 #Algo2Tester version 1.0
 #Repositorio de github: https://github.com/lima-limon-inc/TesterAlgo2
 
+
 if [ ! -f "go.mod" ]
 then
 	echo "
@@ -17,13 +18,26 @@ Joya, volve cuando lo tengas todo listo entonces"
 			[sS]i)
 				echo "Genial. Le voy a poner el nombre de la carpeta en la que estamos de nombre al modulo"
 				echo "Creando modulo"
-				go mod init $(basename $(pwd))
+				modulo=$(basename $(pwd))
+				go mod init $modulo
+				echo "Hago go mod tidy (Por las dudas, nunca viene mal"
+				go mod tidy
+				if [ ! -f "../go.work" ]
+				then
+					echo "
+Ah no, encima te falta el go.work en el directorio padre. Vos no hiciste nada de nada. Acordate la proxima de hacer 'go work init' en el directorio padre antes de empezar"
+					echo "No te preocupues, ya lo hago yo por vos"
+					( cd .. ; go work init )
+				fi
+				echo "Me voy al directorio padre, activo el modulo y vuelvo"
+				( cd .. ; go work use $modulo )
+				echo "Listo todo creado y toda la parte de los modulos 'hecha'
+				"
 				break;;
 			* )
 				echo "No entendi, por favor responde 'no' o 'si'"
-		esac	
+		esac
 	done
-	exit 2 #Paso 2 como error porque el usuario no creo el modulo correspondiente
 fi
 
 if [ -z "$1" ]
