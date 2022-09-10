d42a6e4a9d31f8eb68fa2ce131f2b306fec0279a
Fabri <moboficial00@gmail.com>
2022-09-10 12:58:49 -0300

    Anadi chequeo que el usuario pase un archivo como parametro

--- testear.sh
+++ testear.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+if [ -z "$1" ]
+then
+	echo '''
+Tenes que pasar un archivo como primer parametro para que el programa funcione.
+Ejemplo: ./testear.sh hola_mundo.go
+'''
+	exit 2 #Paso 2 como error porque el usuario no paso los archivos necesarios
+fi
 
 echo "Dandole formato"
 go fmt "$1.go"
