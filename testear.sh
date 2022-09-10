3a36ea0517121d21863eb2e2edd5f78642152ee4
Fabri <moboficial00@gmail.com>
2022-09-10 18:33:19 -0300

    Haciendo los comentarios mas amigables

--- testear.sh
+++ testear.sh
@@ -6,9 +6,9 @@
 if [ ! -f "go.mod" ]
 then
 	echo "
-No te olvides de que en go hay que iniciar el modulo (sea lo que sea que eso signifique). 
+No te olvides de que en go hay que iniciar el modulo (sea lo que sea que eso signifique).
 "
-	while true; do 
+	while true; do
 		read -p "Queres que te ayude a crearlo? (si|no)" rta
 		case $rta in
 			[nN]o)
@@ -20,22 +20,21 @@ Joya, volve cuando lo tengas todo listo entonces"
 				echo "Creando modulo"
 				modulo=$(basename $(pwd))
 				go mod init $modulo
-				echo "Hago go mod tidy (Por las dudas, nunca viene mal"
+				echo "Ejecuto go mod tidy (Por las dudas, nunca viene mal"
 				go mod tidy
 				if [ ! -f "../go.work" ]
 				then
 					echo "
-Ah no, encima te falta el go.work en el directorio padre. Vos no hiciste nada de nada. Acordate la proxima de hacer 'go work init' en el directorio padre antes de empezar"
+Ojo, tene cuidado que te falta archivo go.work en el directorio padre. Eso normalmente se hace ejecutando 'go work init' en el directorio padre"
 					echo "No te preocupues, ya lo hago yo por vos"
 					( cd .. ; go work init )
 				fi
 				echo "Me voy al directorio padre, activo el modulo y vuelvo"
 				( cd .. ; go work use $modulo )
-				echo "Listo todo creado y toda la parte de los modulos 'hecha'
-				"
+				echo "Listo todo creado y toda la parte de los modulos hecha"
 				break;;
 			* )
-				echo "No entendi, por favor responde 'no' o 'si'"
+				echo "Disculpame, no entendi, por favor responde 'no' o 'si'"
 		esac
 	done
 fi
@@ -43,15 +42,15 @@ fi
 if [ -z "$1" ]
 then
 	echo "
-Tenes que pasar un archivo como primer parametro para que el programa funcione.
-Ejemplo: ./testear.sh hola_mundo.go
+Ojo, tenes que pasar un archivo como primer parametro para que el programa funcione.
+Ejemplo: testear.sh hola_mundo.go
 "
 	exit 2 #Paso 2 como error porque el usuario no paso los archivos necesarios
 fi
 
 if [ ! -f "$1" ]
 then
-	echo " 
+	echo "
 ${1} no existe
 	"
 	exit 2 #Paso 2 como error porque el usuario no paso un archivo existente
