eb54cdd688af807e3999239d61392e77bffb5745
Fabri <moboficial00@gmail.com>
2022-09-10 13:42:05 -0300

    Anadi chequeo de que haya un archivo con el cual testear cosas

--- testear.sh
+++ testear.sh
@@ -26,11 +26,20 @@ fi
 
 sinExtension=${1:: -3} #Creo una variable del archivo a compilar sin la extension para facilitar los comandos que le siguen
 
+
 echo "Dandole formato"
 go fmt "$1"
 
 echo "Compilando el programa a ejecutable"
 go build -o "$sinExtension"
+
+if [ ! -f "${sinExtension}_test.go" ]
+then
+	echo #No me gusta nada este echo vacio, lo puse porque no se como lograr usar un echo ''' que pueda imprimir la primera variable pasada al programa $1. Pull requests mas que bienvenidos
+	echo "No hay ningun archivo de testeo. Algo del estilo de ${sinExtension}_test.go"
+	echo
+	exit 2 #Paso 2 como error porque el usuario no paso un archivo sin un test asociado a ese
+fi
 
 #echo "Comienzo del testeo"
 #go test
